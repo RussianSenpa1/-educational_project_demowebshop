55ee55e2ec418809a13b65aa545e002a081c4c43
RussianSenpa1 <milkhailmelnikov@gmail.com>
2022-03-26 17:27:09 +0300

    locators.py - добавлен локатор url страницы регистрации

--- pages/locators.py
+++ pages/locators.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 
 class LinksLocators():
     MAIN_PAGES_LINK = 'http://demowebshop.tricentis.com/'
+    REGISTER_PAGES_LINK = 'http://demowebshop.tricentis.com/register'
 
 class MainPageLocators():
     LOGIN_SELECTOR = (By.XPATH, "//a[@class='ico-register']")
