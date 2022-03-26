79f33d721d25205740b270d7499b3926794917e4
RussianSenpa1 <milkhailmelnikov@gmail.com>
2022-03-26 17:25:20 +0300

    test_main_page.py - добавлен тест проверки url - test_should_see_register_url

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.registration_page import RegistrationPage
 from .pages.locators import LinksLocators
 from .pages.main_page import MainPage
 
@@ -7,3 +8,11 @@ def test_should_see_register_button(browser):  # Проверка есть ли 
     page = MainPage(browser, link)
     page.open()
     page.should_be_register_button()
+
+def test_should_see_register_url(browser):  # Содержит ли url - register при переходе по кнопке "Register"
+    link = LinksLocators.MAIN_PAGES_LINK
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_register_page()
+    register_page = RegistrationPage(browser, browser.current_url)
+    register_page.should_be_register_url()
