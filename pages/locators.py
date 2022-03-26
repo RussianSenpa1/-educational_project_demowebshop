aeed68efc0b80d44f9fdc48eaad0179d3effb244
RussianSenpa1 <milkhailmelnikov@gmail.com>
2022-03-26 15:45:22 +0300

    Добавлен класс локатор ссылок, добавлен локатор ссылки главной страницы, изменен тест наличия кнопки регистрации (добавлен в него локатор ссылки)

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
 
+class LinksLocators():
+    MAIN_PAGES_LINK = 'http://demowebshop.tricentis.com/'
 
 class MainPageLocators():
     LOGIN_SELECTOR = (By.XPATH, "//a[@class='ico-register']")
--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,9 @@
+from .pages.locators import LinksLocators
 from .pages.main_page import MainPage
 
 
 def test_should_see_register_button(browser):  # Проверка есть ли кнопка регистрации на странице
-    link = "http://demowebshop.tricentis.com/"
+    link = LinksLocators.MAIN_PAGES_LINK
     page = MainPage(browser, link)
     page.open()
     page.should_be_register_button()
