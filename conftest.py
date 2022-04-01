f22dae8834f78e3c0639022f0788cdbac500d0d8
RussianSenpa1 <milkhailmelnikov@gmail.com>
2022-04-01 14:55:09 +0300

    Добавлен  аргумент выбора браузера: --browser  (chrome or firefox)

--- conftest.py
+++ conftest.py
@@ -2,8 +2,18 @@ import pytest
 from selenium import webdriver
 
 
+def pytest_addoption(parser):
+    parser.addoption('--browser', action='store', default='chrome',
+                     help="Browser choice. Chrome or Firefox")
+
+
 @pytest.fixture(scope="function")
-def browser():
-    browser = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption("browser")
+    if browser_name == "chrome":
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        browser = webdriver.Firefox()
     yield browser
     browser.quit()
+
