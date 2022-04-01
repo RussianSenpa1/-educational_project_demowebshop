ce00ef159fe28f4ab948417e8a981614d71c4a91
RussianSenpa1 <milkhailmelnikov@gmail.com>
2022-04-01 15:59:54 +0300

    Добавлен  аргумент выбора безголового режима браузера: --headless (on or off)

--- conftest.py
+++ conftest.py
@@ -1,19 +1,41 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
     parser.addoption('--browser', action='store', default='chrome',
-                     help="Browser choice. Chrome or Firefox")
+                     help="Browser choice. chrome or firefox")
+    parser.addoption('--headless', action='store', default='off',
+                     help="Headless choice. on or off")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser")
-    if browser_name == "chrome":
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        browser = webdriver.Firefox()
-    yield browser
-    browser.quit()
+    headless_what = request.config.getoption("headless")
 
+    if browser_name == "chrome":
+        if headless_what == 'off':
+            browser = webdriver.Chrome()
+            yield browser
+            browser.quit()
+
+        elif headless_what == 'on':
+            options = Options()
+            options.add_argument('headless')
+            browser = webdriver.Chrome(options=options)
+            yield browser
+            browser.quit()
+
+    elif browser_name == "firefox":
+        if headless_what == 'off':
+            browser = webdriver.Firefox()
+            yield browser
+            browser.quit()
+        elif headless_what == 'on':
+            options = webdriver.FirefoxOptions()
+            options.headless = True
+            browser = webdriver.Firefox(options=options)
+            yield browser
+            browser.quit()
